d36986e3b8bc0d3cf2a5885d20d29c5c77d03baa
Sirapob PHASUT <omegaooh@gmail.com>
2024-03-12 15:42:34 +0700

    implement UnitTest using Pytest

--- test_circle.py
+++ test_circle.py
@@ -8,5 +8,31 @@ Write unit tests as described in README.md.
 
 """
 from circle import Circle
+import math
+import pytest
+
 
 # TODO write 3 tests as described above
+def test_add_area():
+    """ Test for add_area method"""
+    c1_rad = 2
+    c1 = Circle(c1_rad)
+    c2_rad = 1
+    c2 = Circle(c2_rad)
+    expected_rad = math.hypot(c1_rad,c2_rad)
+    assert c1.add_area(c2).radius == expected_rad
+
+def test_add_area_0():
+    """ Test for add_area when radius is 0"""
+    c1_rad = 2
+    c1 = Circle(c1_rad)
+    c2_rad = 0
+    c2 = Circle(c2_rad)
+    expected_rad = math.hypot(c1_rad, c2_rad)
+    assert c1.add_area(c2).radius == expected_rad
+
+
+def test_add_area_negative():
+    """ Test for add_area when radius is negative"""
+    with pytest.raises(ValueError):
+        Circle(-1)
